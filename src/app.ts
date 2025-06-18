8aa9803360f17c1352a742ae91b12845544747e0
Tevichh <83615857+Tevichh@users.noreply.github.com>
2025-06-18 04:01:28 -0500

    Se aceptan archivos con mayor peso

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,11 @@ import imagesRoute from "./routes/imagesRoute";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
+
+
 
 app.use("/auth", authRoutes)
 app.use("/admin", adminRoutes);
